ed7c70062215dbba678988168fcc754bef2fc46f
aozodbek2197 <aozodbek2197@gmail.com>
2025-12-09 15:35:52 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,33 @@
+# # 8-masala
+# roy = ["dog", "cat", "mouse"]
+# bosh_harf = [i[0] for i in roy]
+# print(bosh_harf)
+# # 9-masala
+# roy = [-5, 3, -1, 0, 7, -2]
+# musbat = [i for i in roy if i >= 0]
+# print(musbat)
+# # 10-masala
+# juft = []
+# toq = []
+# for i in range(1, 11):
+#     if i % 2 == 0:
+#         juft.append(i)
+#     else:
+#         toq.append(i)
+# print(f"juft: {juft}")
+# print(f"toq: {toq}")
+# # 11-masala
+# roy = [4, 9, 16, 25]
+# ildiz = [pow(n, 0.5) for n in roy]
+
+# print(ildiz)
+# # 12-masala
+# roy = []
+# for i in range(51):
+#     if i % 5 == 0:
+#         roy.append(i)
+# print(roy)
+# # 13-masala
+# roy = ["hello", "world", "python"]
+# for i in roy:
+#     print(i.upper())
